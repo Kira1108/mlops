e5ec417bfce23aaf6e3ddaf5c9a578b44768badc
Kira1108 <327485253@163.com>
2022-12-29 08:13:13 +0000

    Initial commit

---
+++ xgboost-serving/client.py
@@ -0,0 +1,53 @@
+import os
+import xgboost as xgb
+import requests
+
+from urllib.parse import urlparse
+from sklearn.datasets import load_svmlight_file
+import requests
+
+
+TRAIN_DATASET_URL = 'https://raw.githubusercontent.com/dmlc/xgboost/master/demo/data/agaricus.txt.train'
+TEST_DATASET_URL = 'https://raw.githubusercontent.com/dmlc/xgboost/master/demo/data/agaricus.txt.test'
+
+
+def _download_file(url: str) -> str:
+    parsed = urlparse(url)
+    file_name = os.path.basename(parsed.path)
+    file_path = os.path.join(os.getcwd(), file_name)
+    
+    if os.path.exists(file_path):
+        return file_path
+    
+    res = requests.get(url)
+    
+    with open(file_path, 'wb') as file:
+        file.write(res.content)
+    
+    return file_path
+
+train_dataset_path = _download_file(TRAIN_DATASET_URL)
+test_dataset_path = _download_file(TEST_DATASET_URL)
+
+# NOTE: Workaround to load SVMLight files from the XGBoost example
+X_train, y_train = load_svmlight_file(train_dataset_path)
+X_test, y_test = load_svmlight_file(test_dataset_path)
+X_train = X_train.toarray()
+X_test = X_test.toarray()
+
+x_0 = X_test[0:1]
+inference_request = {
+    "inputs": [
+        {
+          "name": "predict",
+          "shape": x_0.shape,
+          "datatype": "FP32",
+          "data": x_0.tolist()
+        }
+    ]
+}
+
+endpoint = "http://localhost:8081/v2/models/mushroom-xgboost/versions/v0.1.0/infer"
+response = requests.post(endpoint, json=inference_request)
+
+print(response.json())
